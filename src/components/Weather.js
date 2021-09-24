5b34c6d53963e17aa0d416b6d526f8b36f79a2b5
Sanjana-4 <sanjanasgowda45@gmail.com>
2021-09-24 17:06:42 +0530

    third commit

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, useHistory } from 'react-router-dom'
+import './Weather.css'
 
 
 function Weather() {
@@ -8,22 +9,24 @@ function Weather() {
         history.push("/Winfo");
     }
     return (
-    
-        <div>
-            <center>
                 <div className="weather">
-                <h2 claasName="weather-title">Weather App</h2>
-                <select>
+                    <center>
+                 <form className="form">
+                   <h2>Weather Forecast 🌧   </h2>
+                 <select className="weath_dropdown">
                 <option value ="Chennai">Chennai</option>
                 <option value ="mumbai">Mumbai</option>
                 <option selected value="pune">Pune</option>
                 <option value ="delhi">Delhi</option>
+                <option value ="bangalore">Bangalore</option>
                 </select>
-                <button onClick={handleHistory}>Get Weather</button>
-
+                
+                <button className="button" onClick={handleHistory}>Get Weather</button>
+                </form>
+                </center>
                 </div>
-            </center>
-        </div>
+    
+
 
     )
 }
