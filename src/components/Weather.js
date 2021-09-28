ddeee63dca31ddfce9a37036504680878ccefbb7
Sanjana-4 <sanjanasgowda45@gmail.com>
2021-09-28 16:50:53 +0530

    initial commit

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,26 +4,44 @@ import './Weather.css'
 
 
 function Weather() {
+    
     const history = useHistory();
     const handleHistory = () => {
         history.push("/Winfo");
     }
+    function Validate(){
+         var err = document.getElementById('city').value;
+         if(err == "others"){
+             alert("Select Valid City");
+             return false;
+         }
+         return true;
+    }
+    
     return (
                 <div className="weather">
                     <center>
-                 <form className="form">
+        
+                 <form className="form"  onSubmit={Validate}  >
                    <h2>Weather Forecast 🌧   </h2>
-                 <select className="weath_dropdown">
+                 <select id="city"  className="weath_dropdown">
+                 <option  hidden selected>Select City</option>
                 <option value ="Chennai">Chennai</option>
                 <option value ="mumbai">Mumbai</option>
-                <option selected value="pune">Pune</option>
+                <option  value="pune">Pune</option>
                 <option value ="delhi">Delhi</option>
                 <option value ="bangalore">Bangalore</option>
+                <option value ="others">Others</option>
                 </select>
-                
-                <button className="button" onClick={handleHistory}>Get Weather</button>
+                <br/>
+                <button className="button" onClick={Validate}>Get Weather</button>
+                     
                 </form>
+            
+    
+               
                 </center>
+    
                 </div>
     
 
