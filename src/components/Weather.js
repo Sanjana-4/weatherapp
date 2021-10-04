292f08f6f5de51880e652759c26f44f26489ae28
Sanjana-4 <sanjanasgowda45@gmail.com>
2021-10-04 13:23:58 +0530

    new changes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,50 +1,100 @@
-import React from 'react'
-import { BrowserRouter as Router, useHistory } from 'react-router-dom'
+import React,{useState} from 'react'
+import { useHistory } from 'react-router-dom'
 import './Weather.css'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer,toast } from 'react-toastify'
 
 
+
+const customId = "custom-id-yes";
+
+toast.configure()
 function Weather() {
+      
     
     const history = useHistory();
-    const handleHistory = () => {
-        history.push("/Winfo");
-    }
-    function Validate(){
-         var err = document.getElementById('city').value;
-         if(err == "others"){
-             alert("Select Valid City");
-             return false;
-         }
-         return true;
-    }
-    
-    return (
-                <div className="weather">
-                    <center>
-        
-                 <form className="form"  onSubmit={Validate}  >
-                   <h2>Weather Forecast 🌧   </h2>
-                 <select id="city"  className="weath_dropdown">
-                 <option  hidden selected>Select City</option>
-                <option value ="Chennai">Chennai</option>
-                <option value ="mumbai">Mumbai</option>
-                <option  value="pune">Pune</option>
-                <option value ="delhi">Delhi</option>
-                <option value ="bangalore">Bangalore</option>
-                <option value ="others">Others</option>
-                </select>
-                <br/>
-                <button className="button" onClick={Validate}>Get Weather</button>
-                     
-                </form>
+    function handleHistory () {
+      
+        var err = document.getElementById('city').value;
+       
+        history.push({
             
-    
-               
-                </center>
-    
-                </div>
+            pathname: '/Winfo',
+            state: err,
+            data: data
+            
+        });
+        
+        
+    }
     
 
+
+    const APIKEY = "ea97e52c3dd933f4e06a02012713596a";
+    const [data,setData]=useState([]);
+    const city = useState(('city').value)
+    const weatherdata = (e)=>{
+        e.preventDefault()
+        var err = document.getElementById('city').value;
+        fetch (`http://api.openweathermap.org/data/2.5/weather?q=${err}&appid=${APIKEY}`)
+        .then((response)=> response.json())
+        .then((json)=>{
+          
+            console.log(json);
+            setData(json);
+         })
+    }
+
+    const notify = () =>{
+    toast.warning('Select Valid City',{
+        toastId: customId
+
+
+        });
+    }
+    
+        const diffToast = () => {
+            toast.error('Something went wrong',{
+                toastId:customId
+        });
+    }
+    
+   
+ 
+   
+    
+    
+    return (
+    
+        <div className="weather">
+            <center>
+            <form  className="form" onSubmit={(e)=>weatherdata(e)}   >
+                   <h2>Weather Forecast 🌧   </h2>
+                 <select id="city" className="weath_dropdown" onChange={(e)=>weatherdata(e)} value={('city').value} >
+                 <option hidden value = "Select City">Select City</option>
+                <option value="chennai">Chennai</option>
+                <option value="Mumbai">Mumbai</option>
+                <option value="pune">Pune</option>
+                <option value="Delhi">Delhi</option>
+                <option value="Bangalore">Bangalore</option>
+                </select>
+                <br/>
+                <button className="button" onClick={(e)=>{
+                    var err = document.getElementById('city').value;
+                   if(err === "Select City")
+                   {notify()}
+                else if(data.cod !== 200)
+                {diffToast()}
+                else{handleHistory(e)}
+                 }}>Get Weather</button>
+            <ToastContainer/>
+                </form>
+            </center>
+            <div>
+            
+           
+            </div>
+        </div>
 
     )
 }
